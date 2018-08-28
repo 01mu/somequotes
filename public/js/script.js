c96890ded6bfc30b269b26c866a9b22a7552bb53
01mu <developmentleviathan@gmail.com>
2018-08-28 13:14:44 -0400

    site

---
+++ public/js/script.js
@@ -0,0 +1,64 @@
+const baseURL = 'https://smallfolio.bitnamiapp.com/somequotes/';
+var app = angular.module('textBoxes', []);
+
+app.filter('unsafe', function($sce) {
+    return function(val) {
+        return $sce.trustAsHtml(val);
+    };
+});
+
+app.controller('boxCtrl', ['$scope', '$http', '$rootScope',
+    function($scope, $http, $rootScope) {
+
+    $scope.quotes = [];
+    $scope.button = 'Getting quotes...';
+
+    var url = baseURL + 'get_quotes_random.php?limit=100&start=0';
+
+    update(0);
+
+    $scope.loadMore = function() {
+        $scope.button = 'Loading...';
+        update(1);
+    }
+
+    function authorWP(author) {
+        var authorLink = author.replace(' ', '_');
+        var wpLink = 'http://en.wikipedia.org/wiki/' + authorLink;
+
+        author = '<a href=' + wpLink + ' target="_blank">' + author + '</a>';
+
+        return author;
+    }
+
+    function update(type) {
+        $.getJSON(url, function(json) {
+            var getQuotes = [];
+            var status = json[0].Response;
+            var quotesRespose = json[1];
+            var quotesSize = quotesRespose.length;
+
+            if(status === 'Good') {
+                for(var i = 0; i < quotesSize; i++) {
+                    var quote = '"' + quotesRespose[i].quote + '"';
+                    var author = authorWP(quotesRespose[i].author);
+
+                    var add = {'quote': quote, 'author': '- ' + author};
+
+                    if(type === 0) {
+                        getQuotes.push(add);
+                    } else {
+                        $scope.quotes.push(add);
+                    }
+                }
+            }
+
+            if(type === 0) {
+                $scope.quotes = getQuotes;
+            }
+
+            $scope.button = 'Load more...';
+            $scope.$apply();
+        });
+    }
+}]);
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,67 +1,36 @@
 <!DOCTYPE html>
+
 <html>
 <head>
     <meta charset="utf-8">
     <?php do_includes(); ?>
     <title>Some Quotes</title>
-    <link rel="icon" type="image/png" href="sq.png">
+    <link rel="icon" type="image/png" href="img/sq.png">
     <link href="css/site.css" rel="stylesheet" type="text/css" media="all">
 </head>
 <body>
+    <div class="title">
+        <div class="title-text">
+            Some Quotes
+        </div>
+    </div>
     <div class="container-fluid">
         <div class="body">
             <div ng-app="textBoxes" ng-controller="boxCtrl">
                 <br>
                 <br>
-                <span>
-                    <div ng-repeat="p in quotes" ng-cloak>
-                        <p ng-bind-html="p.quote | unsafe"></p>
-                        <p ng-bind-html="p.author | unsafe"></p>
-                        <br>
-                    </div>
-                </span>
+                <div ng-repeat="p in quotes" ng-cloak>
+                    <p ng-bind-html="p.quote | unsafe"></p>
+                    <p ng-bind-html="p.author | unsafe"></p>
+                    <br>
+                </div>
+                <div class="updateButton" ng-click="loadMore()">
+                    @{{button}}
+                </div>
             </div>
         </div>
     </div>
-    <script>
-const baseURL = 'https://smallfolio.bitnamiapp.com/somequotes/';
-var app = angular.module('textBoxes', []);
-
-app.filter('unsafe', function($sce) {
-    return function(val) {
-        return $sce.trustAsHtml(val);
-    };
-});
-
-app.controller('boxCtrl', ['$scope', '$http', '$rootScope',
-    function($scope, $http, $rootScope) {
-
-    $scope.quotes = [];
-
-        var url = baseURL + 'get_quotes_random.php?limit=50&start=0';
-
-        $.getJSON(url, function(json) {
-            var getQuotes = [];
-            var status = json[0].Response;
-            var quotesRespose = json[1];
-            var quotesSize = quotesRespose.length;
-
-            if(status === 'Good') {
-                for(var i = 0; i < quotesSize; i++) {
-                    var add = {'quote': '"' + quotesRespose[i].quote + '"',
-                        'author': '- ' + quotesRespose[i].author};
-
-                    getQuotes.push(add);
-                }
-            }
-
-            $scope.quotes = getQuotes;
-            $scope.$apply();
-
-        });
-
-}]);
-    </script>
+   <script src="js/script.js"></script>
 </body>
 </html>
 
