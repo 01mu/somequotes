c33f9ca7e5035daed0d313d1cf251d2f000ce777
01mu <developmentleviathan@gmail.com>
2018-08-31 13:55:06 -0400

    l

--- public/js/script.js
+++ public/js/script.js
@@ -14,52 +14,135 @@ app.controller('boxCtrl', ['$scope', '$http', '$rootScope',
     $scope.quotes2 = [];
     $scope.quotes3 = [];
 
+    $scope.authors = [];
+
     $scope.button = 'Getting quotes...';
 
     var trackPage = 0;
     var quoteSearchFlag = 0;
+    var authorSearchFlag = 0;
 
     var url = baseURL + 'get_quotes_random.php?limit=100&start=0';
 
-    update(0);
-
-    //$scope.name='assss';
-    //alert($scope.something);
+    update(0, url);
 
     $scope.loadMore = function() {
-        if(quoteSearchFlag == 1) {
-            trackPage += 100
-
-            url = baseURL + 'get_quotes_search.php?query=' + $scope.qSearch +
-                '&limit=100&start=' + trackPage;
-        }
-
         $scope.button = 'Loading...';
 
-        update(1);
+        if(quoteSearchFlag == 1) {
+            trackPage += 100;
+
+            var url = baseURL + 'get_quotes_search.php?query=' +
+                $scope.qSearch + '&limit=100&start=' + trackPage;
+
+            update(1, url);
+        } else if(authorSearchFlag == 1) {
+            trackPage += 100;
+
+            url = baseURL + 'get_author_search.php?limit=100&start=' +
+                trackPage + '&query=' + $scope.aSearch;
+
+            updateAuthors(1, url);
+        } else {
+            var url = baseURL + 'get_quotes_random.php?limit=100&start=0';
+
+            update(1, url);
+        }
     }
 
-    function authorWP(author) {
-        var authorLink = author.replace(/ /g, '_');
-        var wpLink = 'http://en.wikipedia.org/wiki/' + authorLink;
+    $scope.authorSearch = function() {
+        var url = baseURL + 'get_author_search.php?limit=100&start=0&query=' +
+            $scope.aSearch;
 
-        author = '<a href=' + wpLink + ' target="_blank">' + author + '</a>';
-
-        return author;
-    }
-
-    $scope.quoteSearch = function() {
-        quoteSearchFlag = 1;
-
-        url = baseURL + 'get_quotes_search.php?query=' + $scope.qSearch +
-            '&limit=100&start=' + trackPage;
-
-        update(0);
+        updateAuthors(0, url);
 
         $('html, body').animate({ scrollTop: 0 }, 'fast');
     }
 
-    function update(type) {
+    $scope.quoteSearch = function() {
+        $scope.authors = [];
+        trackPage = 0;
+
+        quoteSearchFlag = 1;
+
+        var url = baseURL + 'get_quotes_search.php?query=' + $scope.qSearch +
+            '&limit=100&start=' + trackPage;
+
+        update(0, url);
+
+        $('html, body').animate({ scrollTop: 0 }, 'fast');
+    }
+
+    $scope.setAuthor = function(guy) {
+        $scope.authors = [];
+
+        var url = baseURL + 'get_author_single.php?author=' + guy.author;
+
+        $.getJSON(url, function(json) {
+            var status = json[0].Response;
+            var authorsResponse = json[1];
+            var authorsSize = authorsResponse.length;
+            var arrFlag = 0;
+
+            if(status === 'Good') {
+                for(var i = 0; i < authorsSize; i++) {
+                    var author = '- ' + authorWP(guy.author);
+                    var quote = '"' + authorsResponse[i]['quote'] + '"';
+
+                    var add = {'author': author, 'quote': quote};
+
+                    switch(arrFlag)
+                    {
+                        case 0: $scope.quotes1.push(add); break;
+                        case 1: $scope.quotes2.push(add); break;
+                        case 2: $scope.quotes3.push(add); break;
+                        default: break;
+                    }
+
+                    arrFlag++;
+
+                    if(arrFlag == 3) {
+                        arrFlag = 0;
+                    }
+                }
+            }
+
+            $scope.$apply();
+        });
+    }
+
+    function updateAuthors(type, url) {
+        $scope.quotes1 = [];
+        $scope.quotes2 = [];
+        $scope.quotes3 = [];
+
+        quoteSearchFlag = 0;
+        authorSearchFlag = 1;
+
+        if(type === 0) {
+            trackPage = 0;
+            $scope.authors = [];
+        }
+
+        $.getJSON(url, function(json) {
+            var status = json[0].Response;
+            var authorsResponse = json[1];
+            var authorsSize = authorsResponse.length;
+
+            if(status === 'Good') {
+                for(var i = 0; i < authorsSize; i++) {
+                    var add = {'author': authorsResponse[i]['author']};
+
+                    $scope.authors.push(add);
+                }
+            }
+
+            $scope.button = 'Load more...';
+            $scope.$apply();
+        });
+    }
+
+    function update(type, url) {
         $.getJSON(url, function(json) {
             var getQuotes1 = [];
             var getQuotes2 = [];
@@ -113,14 +196,17 @@ app.controller('boxCtrl', ['$scope', '$http', '$rootScope',
             $scope.$apply();
         });
     }
-}]);
 
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
+    function ass() {
+        alert('ass');
+    }
+
+    function authorWP(author) {
+        var authorLink = author.replace(/ /g, '_');
+        var wpLink = 'http://en.wikipedia.org/wiki/' + authorLink;
+
+        author = '<a href=' + wpLink + ' target="_blank">' + author + '</a>';
+
+        return author;
+    }
+}]);
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,80 +1,5 @@
 <!DOCTYPE html>
 
-<?php
-/*
-    $url = 'https://smallfolio.bitnamiapp.com/somequotes/';
-    $url = $url . 'get_quotes_random.php?limit=100&start=0';
-
-    $data = file_get_contents($url, false);
-    $posts = json_decode($data, true);
-
-    $size = count($posts[1]);
-
-    $quotes1 = array();
-    $quotes2 = array();
-    $quotes3 = array();
-
-    $flag = 0;
-
-    for($i = 0; $i < $size; $i++)
-    {
-        $quote = '"' . $posts[1][$i]['quote'] . '"';
-        $author = $posts[1][$i]['author'];
-
-        switch($flag)
-        {
-            case 0: $quotes1[] = make_quote($quote, $author, $i); break;
-            case 1: $quotes2[] = make_quote($quote, $author, $i); break;
-            case 2: $quotes3[] = make_quote($quote, $author, $i); break;
-            case 0: $quotes1[] = ['quote' => $quote, 'author' => $author]; break;
-            case 1: $quotes2[] = ['quote' => $quote, 'author' => $author]; break;
-            case 2: $quotes3[] = ['quote' => $quote, 'author' => $author]; break;
-            default: break;
-        }
-
-        $flag++;
-
-        if($flag == 3)
-        {
-            $flag = 0;
-        }
-    }
-
-    function make_quote($quote, $author, $i)
-    {
-        if($i % 2 == 0)
-        {
-            $str =  '<div class="quote-e" >' .
-                    '<p>' . $quote . '</p>' .
-                    '<p>' . $author . '</p>' .
-                    '</div>';
-        }
-        else
-        {
-            $str =  '<div class="quote-o" >' .
-                    '<p>' . $quote . '</p>' .
-                    '<p>' . $author . '</p>' .
-                    '</div>';
-        }
-
-        return $str;
-    }
-
-    function output_quotes($arr)
-    {
-        for($i = 0; $i < count($arr); $i++)
-        {
-            echo $arr[$i];
-            echo '<br>';
-        }
-
-        echo json_encode($arr);
-    }
-
-    //output_quotes($quotes1);
-    */
-?>
-
 <html>
 <head>
     <meta charset="utf-8">
@@ -84,6 +9,8 @@
     <link href="css/site.css" rel="stylesheet" type="text/css" media="all">
 </head>
 <body ng-app="textBoxes" ng-init="something=add" ng-controller="boxCtrl">
+    <div style="margin-bottom: 70px;"></div>
+    <div class="container-fluid">
     <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="navbar-header">
       <a class="navbar-brand" href="#">- Some Quotes -</a>
@@ -101,10 +28,8 @@
         </form>
     </form>
     </nav>
-    <div style="margin-bottom: 40px;"></div>
-    <div class="container-fluid">
         <div class="body">
-            <div class="row">
+            <!--<div class="row">
                 <div class="col-sm-4">
                 <br>
                 <?php //output_quotes($quotes1); ?>
@@ -117,10 +42,20 @@
                 <br>
                 <?php //output_quotes($quotes3); ?>
                 </div>
+            </div>!-->
+            <div ng-repeat="p in authors" ng-cloak>
+                <div ng-if="$odd" class="author-o"
+                    ng-click="$parent.setAuthor(p)">
+                    <p ng-bind-html="p.author | fix"></p>
+                </div>
+                <div ng-if="$even" class="author-e"
+                    ng-click="$parent.setAuthor(p)">
+                    <p ng-bind-html="p.author | fix"></p>
+                </div>
+                <br>
             </div>
             <div class="row">
                 <div class="col-sm-4">
-                    <br>
                     <div ng-repeat="p in quotes1" ng-cloak>
                         <div ng-if="$odd" class="quote-o" >
                             <p ng-bind-html="p.quote | fix"></p>
@@ -134,7 +69,6 @@
                     </div>
                 </div>
                 <div class="col-sm-4">
-                    <br>
                     <div ng-repeat="p in quotes2" ng-cloak>
                         <div ng-if="$odd" class="quote-o" >
                             <p ng-bind-html="p.quote | fix"></p>
@@ -148,7 +82,6 @@
                     </div>
                 </div>
                 <div class="col-sm-4">
-                    <br>
                     <div ng-repeat="p in quotes3" ng-cloak>
                         <div ng-if="$odd" class="quote-o" >
                             <p ng-bind-html="p.quote | fix"></p>
@@ -168,7 +101,7 @@
         </div>
     </div>
     <div class="footer">
-        ass
+        Some Quotes
     </div>
    <script src="js/script.js"></script>
 </body>
@@ -189,3 +122,76 @@ function do_includes()
         'ootstrap/3.3.7/js/bootstrap.min.js"></script>
         ';
 }
+
+/*
+$url = 'https://smallfolio.bitnamiapp.com/somequotes/';
+$url = $url . 'get_quotes_random.php?limit=100&start=0';
+
+$data = file_get_contents($url, false);
+$posts = json_decode($data, true);
+
+$size = count($posts[1]);
+
+$quotes1 = array();
+$quotes2 = array();
+$quotes3 = array();
+
+$flag = 0;
+
+for($i = 0; $i < $size; $i++)
+{
+    $quote = '"' . $posts[1][$i]['quote'] . '"';
+    $author = $posts[1][$i]['author'];
+
+    switch($flag)
+    {
+        case 0: $quotes1[] = make_quote($quote, $author, $i); break;
+        case 1: $quotes2[] = make_quote($quote, $author, $i); break;
+        case 2: $quotes3[] = make_quote($quote, $author, $i); break;
+        case 0: $quotes1[] = ['quote' => $quote, 'author' => $author]; break;
+        case 1: $quotes2[] = ['quote' => $quote, 'author' => $author]; break;
+        case 2: $quotes3[] = ['quote' => $quote, 'author' => $author]; break;
+        default: break;
+    }
+
+    $flag++;
+
+    if($flag == 3)
+    {
+        $flag = 0;
+    }
+}
+
+function make_quote($quote, $author, $i)
+{
+    if($i % 2 == 0)
+    {
+        $str =  '<div class="quote-e" >' .
+                '<p>' . $quote . '</p>' .
+                '<p>' . $author . '</p>' .
+                '</div>';
+    }
+    else
+    {
+        $str =  '<div class="quote-o" >' .
+                '<p>' . $quote . '</p>' .
+                '<p>' . $author . '</p>' .
+                '</div>';
+    }
+
+    return $str;
+}
+
+function output_quotes($arr)
+{
+    for($i = 0; $i < count($arr); $i++)
+    {
+        echo $arr[$i];
+        echo '<br>';
+    }
+
+    echo json_encode($arr);
+}
+
+//output_quotes($quotes1);
+*/
