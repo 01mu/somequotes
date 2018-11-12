ca72a4d4f717950e8469dba16792981973c6941a
01mu <developmentleviathan@gmail.com>
2018-11-12 14:54:26 -0500

    a

--- resources/views/partials/body.blade.php
+++ resources/views/partials/body.blade.php
@@ -2,11 +2,14 @@
     <div ng-if="$odd" class="author-o" ng-click="setAuthor(p.author)">
         <p ng-bind-html="p.author | fix"></p>
     </div>
+
     <div ng-if="$even" class="author-e" ng-click="setAuthor(p.author)">
         <p ng-bind-html="p.author | fix"></p>
     </div>
+
     <br>
 </div>
+
 <div class="row">
     @include('partials/quotes')
 </div>
