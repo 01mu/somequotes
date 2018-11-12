a62cc552a79d4779a5f161e2fbd0a011ae9d00a2
01mu <developmentleviathan@gmail.com>
2018-11-12 14:52:07 -0500

    a

--- resources/views/partials/body.blade.php
+++ resources/views/partials/body.blade.php
@@ -8,7 +8,51 @@
     <br>
 </div>
 <div class="row">
-    @include('quotes')
+    <div class="col-sm-4">
+        <div ng-repeat="p in quotes[0]" ng-cloak>
+            <div ng-if="$odd" class="quote-o"
+                ng-click="setAuthor(p.authorRaw)">
+                <p ng-bind-html="p.quote | fix"></p>
+                <p ng-click="$event.stopPropagation()" class="right" ng-bind-html="p.author | fix"></p>
+            </div>
+            <div ng-if="$even" class="quote-e"
+                ng-click="setAuthor(p.authorRaw)">
+                <p ng-bind-html="p.quote | fix"></p>
+                <p ng-click="$event.stopPropagation()" class="right" ng-bind-html="p.author | fix"></p>
+            </div>
+            <br>
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <div ng-repeat="p in quotes[1]" ng-cloak>
+            <div ng-if="$even" class="quote-o"
+                ng-click="setAuthor(p.authorRaw)">
+                <p ng-bind-html="p.quote | fix"></p>
+                <p ng-click="$event.stopPropagation()" class="right" ng-bind-html="p.author | fix"></p>
+            </div>
+            <div ng-if="$odd" class="quote-e"
+                ng-click="setAuthor(p.authorRaw)">
+                <p ng-bind-html="p.quote | fix"></p>
+                <p ng-click="$event.stopPropagation()" class="right" ng-bind-html="p.author | fix"></p>
+            </div>
+            <br>
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <div ng-repeat="p in quotes[2]" ng-cloak>
+            <div ng-if="$odd" class="quote-o"
+                ng-click="setAuthor(p.authorRaw)">
+                <p ng-bind-html="p.quote | fix"></p>
+                <p ng-click="$event.stopPropagation()" class="right" ng-bind-html="p.author | fix"></p>
+            </div>
+            <div ng-if="$even" class="quote-e"
+                ng-click="setAuthor(p.authorRaw)">
+                <p ng-bind-html="p.quote | fix"></p>
+                <p ng-click="$event.stopPropagation()" class="right" ng-bind-html="p.author | fix"></p>
+            </div>
+            <br>
+        </div>
+    </div>
 </div>
 </div>
 </div>
