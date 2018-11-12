78782e77b93f1fc5eb777cd2f9ada684919946b1
01mu <developmentleviathan@gmail.com>
2018-11-12 15:00:11 -0500

    a

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,8 +7,25 @@
     @include('partials/nav')
     <div class="container-fluid">
         <div class="body">
-            @include('partials/body')
+            <div ng-repeat="p in authors" ng-cloak>
+                <div ng-if="$odd" class="author-o" ng-click="setAuthor(p.author)">
+                    <p ng-bind-html="p.author | fix"></p>
+                </div>
+
+                <div ng-if="$even" class="author-e" ng-click="setAuthor(p.author)">
+                    <p ng-bind-html="p.author | fix"></p>
+                </div>
+
+                <br>
+            </div>
+
+            <div class="row">
+                @include('partials/quotes')
+            </div>
         </div>
+    </div>
+    <div class="header" ng-hide="hideRelations" ng-cloak>
+        @{{relation}}
     </div>
     <div class="container-fluid">
         <div class="body">
--- resources/views/partials/body.blade.php
+++ resources/views/partials/body.blade.php
@@ -1,19 +1,1 @@
-<div ng-repeat="p in authors" ng-cloak>
-    <div ng-if="$odd" class="author-o" ng-click="setAuthor(p.author)">
-        <p ng-bind-html="p.author | fix"></p>
-    </div>
 
-    <div ng-if="$even" class="author-e" ng-click="setAuthor(p.author)">
-        <p ng-bind-html="p.author | fix"></p>
-    </div>
-
-    <br>
-</div>
-
-<div class="row">
-    @include('partials/quotes')
-</div>
-
-<div class="header" ng-hide="hideRelations" ng-cloak>
-    @{{relation}}
-</div>
